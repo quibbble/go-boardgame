ab52069dd8badcec76915f3d7cae6271388608e0
freglyc <cdf7@rice.edu>
2021-10-23 14:50:35 -0700

    update error name

--- pkg/bgerr/error.go
+++ pkg/bgerr/error.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-// BoardGameError provides a simple method of returning common board game errors to reduce duplicate code
-type BoardGameError struct {
+// Error provides a simple method of returning common board game errors to reduce duplicate code
+type Error struct {
 	Err    error
 	Status int
 }
 
-func (i *BoardGameError) Error() string {
+func (i *Error) Error() string {
 	return fmt.Sprintf("%s: %s", StatusText(i.Status), i.Err.Error())
 }
