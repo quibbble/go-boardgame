c3a17a8b241a175ecaff9e3e9d172024cbdd128c
freglyc <cdf7@rice.edu>
2021-10-31 14:14:41 -0700

    add advanced options

--- boardgame.go
+++ boardgame.go
@@ -3,22 +3,21 @@ package go_boardgame
 // BoardGame is a representation of a board game allowing one to perform actions on the game as well as to retrieve game data
 type BoardGame interface {
 	// Do performs an action on the game
-	Do(action BoardGameAction) error
+	Do(action *BoardGameAction) error
 
 	// GetSnapshot retrieves the current game state from 'team' view
 	// Entering nothing returns a complete snapshot i.e. all hands, resources, etc.
 	// Entering more than one team will error
 	GetSnapshot(team ...string) (*BoardGameSnapshot, error)
+}
 
-	// GetNotation gets the game notation required to load this game at a later time
-	// A notation is the game seed and a simplified list of actions done to reach the current game state
-	// Notation - "'seed':'team index','action index','details one','details two',...;..."
-	// EX: "123:0,0,0,F;1,1;" seed is 123, team 0 played action 0 with details 0 and F, team 1 played action 1
-	// todo need a way to store BordGameOptions in this notation
+// AdvancedBoardGame provides additional functionality that is not required to play a game but can be useful in many cases
+type AdvancedBoardGame interface {
+	BoardGame
+
+	// GetNotation returns a simplified notation allowing for easy storage and retrieval of games
+	// This notation can be defined as desired but below is a simple example of a potential implementation
+	// "'num teams':'seed':'create option details':'team num','action num','details','details',...;..."
+	// EX: "2:123::0,0,0,F;1,1;" 2 teams, seed is 123, no extra options, team 0 played action 0 with details 0 and F, team 1 played action 1
 	GetNotation() string
-
-	// GetSeed returns the seed used to generate randomness in the game
-	// Useful for recreating games for replays or rollbacks using the seed and list of game actions
-	// Some games do not require randomness in which case zero may be returned
-	GetSeed() int64
 }
--- builder.go
+++ builder.go
@@ -2,16 +2,18 @@ package go_boardgame
 
 // BoardGameBuilder builds a game given a set of options
 type BoardGameBuilder interface {
-	// Create creates a game with desired options and an optional random seed
-	// Leaving seed unset will typically result in the seed being set to time.Now().UnixNano()
-	// Seed is used for deterministic randomness allowing for features such as replays or rollbacks of games
-	Create(options BoardGameOptions, seed ...int64) (BoardGame, error)
-
-	// Load takes a game notation and rebuilds the game
-	// The notation can load both complete and incomplete games
-	// If the game is incomplete it can be continued to be played from where it left off
-	Load(notation string) (BoardGame, error)
+	// Create creates a game with desired options
+	Create(options *BoardGameOptions) (BoardGame, error)
 
 	// Key gets the game's unique key, typically the name of the game
 	Key() string
+}
+
+// AdvancedBoardGameBuilder provides additional functionality that is not required to build a game but can be useful in many cases
+type AdvancedBoardGameBuilder interface {
+	BoardGameBuilder
+
+	// Load takes a list of teams and game notation and rebuilds the game up to the point defined in the notation
+	// Length of teams must match the number of teams defined in notation
+	Load(teams []string, notation string) (BoardGame, error)
 }
