c16f303da6f4086cd47fb5e92dcd6ad69238d6b0
freglyc <cdf7@rice.edu>
2021-10-31 00:31:14 -0700

    begin notation

--- boardgame.go
+++ boardgame.go
@@ -10,6 +10,13 @@ type BoardGame interface {
 	// Entering more than one team will error
 	GetSnapshot(team ...string) (*BoardGameSnapshot, error)
 
+	// GetNotation gets the game notation required to load this game at a later time
+	// A notation is the game seed and a simplified list of actions done to reach the current game state
+	// Notation - "'seed':'team index','action index','details one','details two',...;..."
+	// EX: "123:0,0,0,F;1,1;" seed is 123, team 0 played action 0 with details 0 and F, team 1 played action 1
+	// todo need a way to store BordGameOptions in this notation
+	GetNotation() string
+
 	// GetSeed returns the seed used to generate randomness in the game
 	// Useful for recreating games for replays or rollbacks using the seed and list of game actions
 	// Some games do not require randomness in which case zero may be returned
--- builder.go
+++ builder.go
@@ -7,6 +7,11 @@ type BoardGameBuilder interface {
 	// Seed is used for deterministic randomness allowing for features such as replays or rollbacks of games
 	Create(options BoardGameOptions, seed ...int64) (BoardGame, error)
 
+	// Load takes a game notation and rebuilds the game
+	// The notation can load both complete and incomplete games
+	// If the game is incomplete it can be continued to be played from where it left off
+	Load(notation string) (BoardGame, error)
+
 	// Key gets the game's unique key, typically the name of the game
 	Key() string
 }
